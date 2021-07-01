69fb0b0dd561ddd109edf2939600b7334b3ba322
Zexa <zexa@cock.li>
2021-07-01 18:54:54 +0300

    Finalize mock endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use tide::prelude::*;
 use tide::Request;
 use uwuifier::uwuify_str_sse;
+use tide::Body;
+use tide::Response;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -14,8 +16,39 @@ async fn main() -> tide::Result<()> {
 // HTTP/GET /pokemon/translated/:pokemon_name
 // http://localhost:5000/pokemon/translated/mewtwo
 async fn pokemon_translated(mut req: Request<()>) -> tide::Result {
-    // TODO: If we had very long strings we could probably spawn a thread for this.
-    // though uwuify is extremely fast so maybe unnecessary.
-    Ok(uwuify_str_sse("hello world").into())
+    let mut pokemon = get_pokemon(req.param("pokemon_name")?).await?;
+    pokemon.change_description(uwuify_str_sse(pokemon.description()));
+
+    let mut response = Response::new(200);
+    response.set_body(Body::from_json(&pokemon)?);
+
+    Ok(response)
+}
+
+#[derive(Serialize)]
+struct Pokemon {
+    name: String,
+    description: String,
+    habitat: String,
+    is_legendary: bool,
+}
+
+impl Pokemon {
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+
+    pub fn change_description(&mut self, description: String) {
+        self.description = description;
+    }
+}
+
+async fn get_pokemon(name: &str) -> std::io::Result<Pokemon> {
+    Ok(Pokemon {
+        name: "Charizard".to_string(),
+        description: "Best collectible".to_string(),
+        habitat: "Caves, I guess".to_string(),
+        is_legendary: false,
+    })
 }
 
